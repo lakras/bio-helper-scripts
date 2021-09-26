788360fedc5fb9f89089609f14bb97b0cf69ccb3
Lydia Andreyevna Krasilnikova <lakras@mit.edu>
2021-09-26 17:18:14 -0400

    added not equal to filter option

--- tables/filter_table_rows_by_column_value.pl
+++ tables/filter_table_rows_by_column_value.pl
@@ -6,7 +6,7 @@
 
 # Usage:
 # perl filter_table_rows_by_column_value.pl [tab-separated table]
-# [0 to match cells containing query, 1: beginning with, 2: ending with, 3: equal to]
+# [0 to match cells containing query, 1: beginning with, 2: ending with, 3: equal to, 4: not equal to]
 # "[title of column to filter by]" "[column value to select]"
 
 # Prints to console. To print to file, use
@@ -20,7 +20,7 @@ use warnings;
 
 
 my $table = $ARGV[0];
-my $option = $ARGV[1]; # 0 to match cells containing query, 1: beginning with, 2: ending with, 3: equal to
+my $option = $ARGV[1]; # 0 to match cells containing query, 1: beginning with, 2: ending with, 3: equal to, 4: not equal to
 my $title_of_column_to_filter_by = $ARGV[2];
 my $column_value_to_select = $ARGV[3];
 
@@ -90,6 +90,7 @@ while(<TABLE>) # for each row in the file
 				or !length($column_value) and !length($column_value_to_select) # both empty strings
 				or $column_value and $column_value_to_select
 					and ($option == 3 and $column_value eq $column_value_to_select    # 3 to match cells equal to query
+					or $option == 4 and $column_value ne $column_value_to_select      # 4 to match cells not equal to query
 					or $option == 0 and $column_value =~ /$column_value_to_select/    # 0 to match cells containing query
 					or $option == 1 and $column_value =~ /^$column_value_to_select/   # 1 to match cells beginning with query
 					or $option == 2 and $column_value =~ /$column_value_to_select$/)) # 2 to match cells ending with query
