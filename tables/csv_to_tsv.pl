8449409f95725757b974c300da31af77d8548f70
Lydia Andreyevna Krasilnikova <lakras@mit.edu>
2022-02-24 13:35:20 -0500

    uses single reserved character to replace initial delimiter inside quotes instead of using integer

--- tables/csv_to_tsv.pl
+++ tables/csv_to_tsv.pl
@@ -21,6 +21,23 @@ my $PREVIOUS_DELIMITER = ",";
 my $NEW_DELIMITER = "\t";
 my $ESCAPE_QUOTATIONS_CHARACTER = "\"";
 
+my @RESERVED_CHARACTER_OPTIONS = (
+	"¤", "¶", "§", "Ç", "ü", "é", "â", "ä", "à", "å", "ç", "ê", "ë", "è", "ï", "î", "ì",
+	"æ", "Æ", "ô", "ö", "ò", "û", "ù", "ÿ", "¢", "£", "¥", "ƒ", "á", "í", "ó", "ú", "ñ",
+	"Ñ", "‡", "‡", "¿", "¬", "½", "¼", "¡", "«", "»", "¦", "ß", "µ", "±", "°", "•", "·",
+	"²", "€", "„", "…", "†", "‡", "ˆ", "‰", "Š", "‹", "Œ", "‘", "’", "“", "”", "–", "—",
+	"˜", "™", "š", "›", "œ", "Ÿ", "¨", "Character	Sequence", "©", "®", "¯", "³", "´",
+	"¸", "¹", "¾", "À", "Á", "Â", "Ã", "Ä", "Å", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï",
+	"Ð", "Ò", "Ó", "Ô", "Õ", "Ö", "×", "Ø", "Ù", "Ú", "Û", "Ü", "Ý", "Þ", "ã", "ð", "õ",
+	"÷", "ø", "ü", "ý", "þ","←", "↑", "→", "↓", "↔", "↕", "↖", "↗", "↘", "↙", "↚", "↛",
+	"↜", "↝", "↞", "↟", "↠", "↡", "↢", "↣", "↤", "↥", "↦", "↧", "↨", "↩", "↪", "↫", "↬",
+	"↭", "↮", "↯", "↰", "↱", "↲", "↳", "↴", "↵", "↶", "↷", "↸", "↹", "↺", "↻", "↼", "↽",
+	"↾", "↿", "⇀", "⇁", "⇂", "⇃", "⇄", "⇅", "⇆", "⇇", "⇈", "⇉", "⇊", "⇋", "⇌", "⇍", "⇎",
+	"⇏", "⇐", "⇑", "⇒", "⇓", "⇔", "⇕", "⇖", "⇗", "⇘", "⇙", "⇚", "⇛", "⇜", "⇝", "⇞", "⇟",
+	"⇠", "⇡", "⇢", "⇣", "⇤", "⇥", "⇦", "⇧", "⇨", "⇩", "⇪", "⇫", "⇬", "⇭", "⇮", "⇯", "⇰",
+	"⇱", "⇲", "⇳", "⇴", "⇵", "⇶", "⇷", "⇸", "⇹", "⇺", "⇻", "⇼", "⇽", "⇾", "⇿"
+);
+
 
 # verifies that input file exists and is not empty
 if(!$table or !-e $table or -z $table)
@@ -39,7 +56,7 @@ while(<TABLE>) # for each row in the file
 	my $line = $_;
 	
 	# selects reserved character or character set
-	my $reserved_character = generate_character_string_not_in_input_string($line);
+	my $reserved_character = generate_character_not_in_input_string($line);
 	if($line =~ /$reserved_character/)
 	{
 		print STDERR "Error: line contains selected reserved character "
@@ -47,7 +64,25 @@ while(<TABLE>) # for each row in the file
 		die;
 	}
 	
-	# replaces all commas within quotes with reserved character
+	# replaces all commas within quotes with reserved character, in large groups of 8 first
+	while($line =~ /$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/)
+	{
+		$line =~ s/$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/$ESCAPE_QUOTATIONS_CHARACTER$1$reserved_character$2$reserved_character$3$reserved_character$4$reserved_character$5$reserved_character$6$reserved_character$7$reserved_character$8$reserved_character$9$ESCAPE_QUOTATIONS_CHARACTER/g;
+	}
+	
+	# replaces all commas within quotes with reserved character, in groups of 5
+	while($line =~ /$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/)
+	{
+		$line =~ s/$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/$ESCAPE_QUOTATIONS_CHARACTER$1$reserved_character$2$reserved_character$3$reserved_character$4$reserved_character$5$reserved_character$6$ESCAPE_QUOTATIONS_CHARACTER/g;
+	}
+	
+	# replaces all commas within quotes with reserved character in groups of 2
+	while($line =~ /$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/)
+	{
+		$line =~ s/$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/$ESCAPE_QUOTATIONS_CHARACTER$1$reserved_character$2$reserved_character$3$ESCAPE_QUOTATIONS_CHARACTER/g;
+	}
+	
+	# replaces all remaining commas within quotes with reserved character
 	while($line =~ /$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)$ESCAPE_QUOTATIONS_CHARACTER/)
 	{
 		$line =~ s/$ESCAPE_QUOTATIONS_CHARACTER(.*)$PREVIOUS_DELIMITER(.*)"/"$1$reserved_character$2$ESCAPE_QUOTATIONS_CHARACTER/g;
@@ -70,15 +105,17 @@ close TABLE;
 
 
 # generates string of characters not found in input string
-sub generate_character_string_not_in_input_string
+sub generate_character_not_in_input_string
 {
 	my $input_string = $_[0];
-	my $absent_character_string = 0;
-	while($input_string =~ /$absent_character_string/)
+	foreach my $reserved_character_option(@RESERVED_CHARACTER_OPTIONS)
 	{
-		$absent_character_string++;
+		if($input_string !~ /$reserved_character_option/)
+		{
+			return $reserved_character_option;
+		}
 	}
-	return $absent_character_string;
+	return 0; # didn't find a character not in input string
 }
 
 
