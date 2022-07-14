f23be5fcfd5a153388347aeca3d0984c23dbf236
Lydia Andreyevna Krasilnikova <lakras@mit.edu>
2022-07-14 18:58:54 -0400

    fixed naming and deleted commented out code

--- misc/bulk_diff_same_name_files_in_two_directories.pl
+++ misc/bulk_diff_same_name_files_in_two_directories.pl
@@ -28,7 +28,6 @@ if($directory_2 !~ /\/$/)
 
 
 # retrieves all filepaths in first directory
-# my @filepaths_in_directory_1 = (); # list of all filepaths appearing in directory 1
 my %filename_appears_in_directory_1 = (); # key: file name -> value: 1 if file appears in directory 1
 opendir DIRECTORY_1, $directory_1;
 while(my $file = readdir(DIRECTORY_1))
@@ -40,7 +39,6 @@ close DIRECTORY_1;
 
 
 # retrieves all filepaths in second directory
-# my @filepaths_in_directory_2 = (); # list of all filepaths appearing in directory 1
 my %filename_appears_in_directory_2 = (); # key: file name -> value: 1 if file appears in directory 2
 opendir DIRECTORY_2, $directory_2;
 while(my $file = readdir(DIRECTORY_2))
@@ -51,19 +49,14 @@ while(my $file = readdir(DIRECTORY_2))
 close DIRECTORY_2;
 
 
-# sorts filepaths from the two directories
-@filepaths_in_directory_1 = sort @filepaths_in_directory_1;
-@filepaths_in_directory_2 = sort @filepaths_in_directory_2;
-
-
 # compares each pair of same-name files, one from each directory
 foreach my $file_name(sort keys %filename_appears_in_directory_1)
 {
 	if($filename_appears_in_directory_2{$file_name}) # file name appears in both directories
 	{
 		# generates file paths for each directory
-		my $filepath_1 = $directory_1.$file;
-		my $filepath_2 = $directory_2.$file;
+		my $filepath_1 = $directory_1.$file_name;
+		my $filepath_2 = $directory_2.$file_name;
 		
 		# compares the two files
 		`diff $filepath_1 $filepath_2`;
