bddc740834c2c27b18f387b5a1193f8e3c5179d3
Lydia Andreyevna Krasilnikova <lakras@mit.edu>
2023-01-08 12:57:31 -0500

    added DIAMOND columns option

--- blast/retrieve_top_blast_hits_for_each_sequence.pl
+++ blast/retrieve_top_blast_hits_for_each_sequence.pl
@@ -5,9 +5,11 @@
 
 # Usage:
 # perl retrieve_top_blast_hits_for_each_sequence.pl [blast output]
+# [1 to treat blast output as modified DIAMOND output]
 
 # Prints to console. To print to file, use
 # perl retrieve_top_blast_hits_for_each_sequence.pl [blast output]
+# [1 to treat blast output as modified DIAMOND output]
 # > [output subset of blast output table]
 
 
@@ -16,6 +18,7 @@ use warnings;
 
 
 my $blast_output = $ARGV[0]; # format: qseqid sacc stitle staxids sscinames sskingdoms qlen slen length pident qcovs evalue
+my $is_diamond_output = $ARGV[1]; # if 1, treats blast output as DIAMOND output; format: qseqid stitle part 1: accession number stitle part 2: sequence name qlen slen length pident qcovhsp evalue
 
 
 my $NO_DATA = "NA";
@@ -24,13 +27,36 @@ my $DELIMITER = "\t";
 my $TAXONID_SEPARATOR = ";"; # in blast file
 
 
-# blast file
-my $SEQUENCE_NAME_COLUMN = 0; 	# qseqid
-my $MATCHED_TAXONID_COLUMN = 3;	# staxids (Subject Taxonomy ID(s), separated by a ';')
-my $PERCENT_ID_COLUMN = 9; 		# pident
-my $QUERY_COVERAGE_COLUMN = 10;	# qcovs
-my $EVALUE_COLUMN = 11;			# evalue
+# blast file columns
+# format: qseqid sacc stitle staxids sscinames sskingdoms qlen slen length pident qcovs evalue
+my $SEQUENCE_NAME_COLUMN_BLAST = 0; 	# qseqid
+my $PERCENT_ID_COLUMN_BLAST = 9; 		# pident
+my $QUERY_COVERAGE_COLUMN_BLAST = 10;	# qcovs
+my $EVALUE_COLUMN_BLAST = 11;			# evalue
 
+# modified DIAMOND output file columns
+# format: qseqid stitle part 1: accession number stitle part 2: sequence name qlen slen length pident qcovhsp evalue
+# Note: sstitle column must first be separated out into two columns, accession number and sequence name
+# perl split_column_after_query.pl results/sample.fasta_DIAMOND_nr.out 1 " " > results/sample.fasta_DIAMOND_nr.out_match_column_split.txt
+my $SEQUENCE_NAME_COLUMN_DIAMOND = 0; 	# qseqid
+my $PERCENT_ID_COLUMN_DIAMOND = 6; 		# pident
+my $QUERY_COVERAGE_COLUMN_DIAMOND = 7;	# qcovs
+my $EVALUE_COLUMN_DIAMOND = 8;			# evalue
+
+
+# determines whether columns should be blast format or modified diamond format
+my $sequence_name_column = $SEQUENCE_NAME_COLUMN_BLAST; 	# qseqid
+my $percent_id_column = $PERCENT_ID_COLUMN_BLAST; 			# pident
+my $query_coverage_column = $QUERY_COVERAGE_COLUMN_BLAST;	# qcovs
+my $evalue_column = $EVALUE_COLUMN_BLAST;					# evalue
+if($is_diamond_output)
+{
+	$sequence_name_column = $SEQUENCE_NAME_COLUMN_DIAMOND; 		# qseqid
+	$percent_id_column = $PERCENT_ID_COLUMN_DIAMOND; 			# pident
+	$query_coverage_column = $QUERY_COVERAGE_COLUMN_DIAMOND;	# qcovs
+	$evalue_column = $EVALUE_COLUMN_DIAMOND;					# evalue
+
+}
 
 # verifies that input file exists and is not empty
 if(!$blast_output or !-e $blast_output or -z $blast_output)
@@ -52,10 +78,10 @@ while(<BLAST_OUTPUT>)
 	if($_ =~ /\S/)
 	{
 		my @items = split($DELIMITER, $_);
-		my $sequence_name = $items[$SEQUENCE_NAME_COLUMN];
-# 		my $percent_id = $items[$PERCENT_ID_COLUMN];
-# 		my $query_coverage = $items[$QUERY_COVERAGE_COLUMN];
-		my $evalue = $items[$EVALUE_COLUMN];
+		my $sequence_name = $items[$sequence_name_column];
+# 		my $percent_id = $items[$percent_id_column];
+# 		my $query_coverage = $items[$query_coverage_column];
+		my $evalue = $items[$evalue_column];
 		
 		# new sequence, so at least the first match has lowest e-value
 		if($sequence_name ne $previous_sequence_name)
